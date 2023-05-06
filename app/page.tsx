5313d827d25a729031e3c17a1e038b0379c0d047
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-05-06 11:13:01 +0530

    Wrap everything in suspense

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,32 @@ import HeroSection from "@/components/hero/HeroSection";
 import JourneySection from "@/components/journey/JourneySection";
 import ProjectSection from "@/components/projects/ProjectSection";
 import SkillsSection from "@/components/skills/SkillsSection";
+import { Suspense } from "react";
 
 export default function Home() {
 	return (
 		<main className="bg-[#090A0F]">
-			<Snow />
-			<HeroSection />
-			<SkillsSection />
-			<ProjectSection />
-			<CertificationSection />
-			<JourneySection />
-			<ContactSection />
+			<Suspense>
+				<Snow />
+			</Suspense>
+			<Suspense>
+				<HeroSection />
+			</Suspense>
+			<Suspense>
+				<SkillsSection />
+			</Suspense>
+			<Suspense>
+				<ProjectSection />
+			</Suspense>
+			<Suspense>
+				<CertificationSection />
+			</Suspense>
+			<Suspense>
+				<JourneySection />
+			</Suspense>
+			<Suspense>
+				<ContactSection />
+			</Suspense>
 		</main>
 	);
 }
