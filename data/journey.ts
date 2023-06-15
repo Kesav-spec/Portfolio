a4e7b7740824beaff0250c3d45ab856ef14e1e1b
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-06-15 18:53:24 +0530

    Update journey

--- data/journey.ts
+++ data/journey.ts
@@ -1,8 +1,8 @@
 export const journey: Journey[] = [
 	{
 		id: 1,
-		title: "Bachelor of Technology (Information Technology)",
-		duration: "2019 - Present",
+		title: "Bachelor of Technology in Information Technology",
+		duration: "July 2019 - July 2023",
 		location: "Anna University",
 		achievements: [],
 	},
