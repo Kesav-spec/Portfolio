e882f74408fa49c4b4fea9149be285cdfe8f9410
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-07-11 14:40:23 +0530

    Add wantedly as a social link

--- components/hero/HeroSection.tsx
+++ components/hero/HeroSection.tsx
@@ -1,6 +1,7 @@
 import ScrollIndicator from "./ScrollIndicator";
 import SocialLink from "./SocialLink";
-import { FiGithub, FiLinkedin, FiMail, FiFileText } from "react-icons/fi";
+import { SiGithub, SiLinkedin, SiWantedly } from "react-icons/si";
+import { FiMail, FiFileText } from "react-icons/fi";
 
 export default function HeroSection() {
 	return (
@@ -12,17 +13,22 @@ export default function HeroSection() {
 					</span>
 				</div>
 				<div className="mt-12 flex w-full animate-[show_.5s_2.5s_ease-in-out_forwards] justify-evenly opacity-0">
+					<SocialLink text="E-Mail" link="mailto:nishanth.rj@proton.me" Icon={FiMail} />
+					<SocialLink
+						text="LinkedIn"
+						link="https://www.linkedin.com/in/nishanth-rj"
+						Icon={SiLinkedin}
+					/>
 					<SocialLink
 						text="Github"
 						link="https://github.com/nishanthrj"
-						Icon={FiGithub}
+						Icon={SiGithub}
 					/>
 					<SocialLink
-						text="LinkedIn"
-						link="https://www.linkedin.com/in/nishanth-rj/"
-						Icon={FiLinkedin}
+						text="Wantedly"
+						link="https://www.wantedly.com/id/nishanthrj"
+						Icon={SiWantedly}
 					/>
-					<SocialLink text="E-Mail" link="mailto:nishanth.rj@proton.me" Icon={FiMail} />
 					<SocialLink
 						text="Resume"
 						link="/assets/hero/Nishanth R J - Resume.pdf"
