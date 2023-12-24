33dace92f82b3975993a54ea073300aae113c8ca
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-12-24 18:58:08 +0530

    Create a japanese version of all data

--- data/journey.ts
+++ data/journey.ts
@@ -1,9 +1,18 @@
-export const journey: Journey[] = [
+export const journey_en: Journey[] = [
 	{
 		id: 1,
 		title: "Bachelor of Technology in Information Technology",
 		duration: "Aug 2019 - Jul 2023",
 		location: "Anna University",
+		achievements: [],
+	},
+];
+export const journey_jp: Journey[] = [
+	{
+		id: 1,
+		title: "情報技術学士",
+		duration: "2019年8月 - 2023年7月",
+		location: "アンナ大学",
 		achievements: [],
 	},
 ];
--- data/skills.ts
+++ data/skills.ts
@@ -1,4 +1,4 @@
-export const skills: Skills = {
+export const skills_en: Skills = {
 	"Programming Languages": ["Python", "JavaScript", "TypeScript"],
 	// Frameworks: ["React", "Next.js"],
 	Frontend: ["React.js", "Next.js", "HTML & CSS", "Sass", "TailwindCSS"],
@@ -7,4 +7,15 @@ export const skills: Skills = {
 	Services: ["Supabase", "IBMCloud", "Vercel"],
 	Tools: ["Git", "Docker"],
 	Design: ["Photoshop", "Figma"],
+};
+
+export const skills_jp = {
+	プログラミング言語: ["Python", "JavaScript", "TypeScript"],
+	// フレームワーク: ["React", "Next.js"],
+	フロントエンド: ["React.js", "Next.js", "HTML＆CSS", "Sass", "TailwindCSS"],
+	バックエンド: ["Node.js", "Django", "Flask", "FastAPI"],
+	データベース: ["PostgreSQL", "MySQL", "MongoDB", "Redis"],
+	サービス: ["Supabase", "IBM Cloud", "Vercel"],
+	ツール: ["Git", "Docker"],
+	デザイン: ["Photoshop", "Figma"],
 };
