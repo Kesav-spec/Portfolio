cdcbc5e2459106ed787dd2a3f22486f8e2ba1775
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2023-12-25 12:19:07 +0530

    Fix typo

--- components/certifications/CertificateCard.tsx
+++ components/certifications/CertificateCard.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function CerticateCard({ title, link, image, issuer, date }: Certificate) {
+export default function CertificateCard({ title, link, image, issuer, date }: Certificate) {
 	return (
 		<Link className="flex flex-col gap-4 text-white no-underline" href={link} target="_blank">
 			<div className="flex h-min flex-shrink-0 flex-col items-center overflow-hidden">
--- components/certifications/CertificationSection.tsx
+++ components/certifications/CertificationSection.tsx
@@ -1,5 +1,5 @@
 import Section from "../common/Section";
-import CerticateCard from "./CerticateCard";
+import CertificateCard from "./CertificateCard";
 import { certificates } from "@/data/certificates";
 
 export default function CertificationSection() {
@@ -7,7 +7,7 @@ export default function CertificationSection() {
 		<Section title="certifications">
 			<div className="mt-12 grid w-full grid-cols-[repeat(auto-fill,minmax(10rem,min-content))] justify-center gap-x-16 gap-y-16 pt-8 md:grid-cols-[repeat(auto-fill,minmax(12rem,min-content))] xl:grid-cols-[repeat(auto-fill,minmax(15rem,min-content))]">
 				{certificates.map((cert, idx) => (
-					<CerticateCard key={idx} {...cert} />
+					<CertificateCard key={idx} {...cert} />
 				))}
 			</div>
 		</Section>
