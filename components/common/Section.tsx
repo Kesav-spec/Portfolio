0276acf4c4d53efaf9d8d957885763765758cd00
Nishanth R J <59363325+nishanthrj@users.noreply.github.com>
2024-01-01 12:09:38 +0530

    Fix animation

--- components/common/Section.tsx
+++ components/common/Section.tsx
@@ -8,9 +8,9 @@ export default function Section({ title, children }: Readonly<SectionProps>) {
 	return (
 		<section
 			ref={ref}
-			className={`mb-48 mt-48 flex flex-col items-center text-[#ECECEC] ${
+			className={`mb-48 flex flex-col items-center text-[#ECECEC] opacity-0 ${
 				inView && "animate__fadeIn"
-			}`}>
+			} ${title !== "skills" && title !== "スキル" && "mt-48"}`}>
 			<h1
 				className={`block w-4/5 border-b border-[#FFF] pb-4 text-center text-4xl font-normal uppercase tracking-[0.35rem] `}>
 				{title}
