e8708285687b4e77a7215c64fea6050312add89c
Kesav S J <75158817+Kesav-spec@users.noreply.github.com>
2024-01-10 16:16:29 +0000

    achieve

--- data/journey.ts
+++ data/journey.ts
@@ -12,7 +12,9 @@ export const journeyEN: Journey[] = [
 		title: "Bachelor of Technology in Information Technology",
 		duration: "Aug 2019 - Jul 2023",
 		location: "Sri Venkateswara College of Enginineering, Anna University, India",
-		achievements: [],
+		achievements: ["Awarded in the Ease the Error Hackathons by IETE Chennai Centre (Sep 2020 and May 2021)",
+		"Awarded third place in a 24-hour online Industry associated inter-college level Hackathon organised by the Department of Information Technology.",
+		"Selected for incubation and secured a 10,000 INR seed fund from Mr. Sivarajah Ramanathan, the Mission Director & CEO of the Tamil Nadu Startup and Innovation Mission, during a pitching event hosted by Entrepreneurship Promotion and Incubation Centre (EPIC) at Sri Venkateswara College of Engineering."],
 	},
 	
 ];
@@ -30,6 +32,8 @@ export const journeyGA: Journey[] = [
 		title: "Bachelor of Technology in Information Technology",
 		duration: "Aug 2019 - Jul 2023",
 		location: "Sri Venkateswara College of Enginineering, Anna University, India",
-		achievements: [],
+		achievements: ["Awarded in the Ease the Error Hackathons by IETE Chennai Centre (Sep 2020 and May 2021)",
+		"Awarded third place in a 24-hour online Industry associated inter-college level Hackathon organised by the Department of Information Technology.",
+		"Selected for incubation and secured a 10,000 INR seed fund from Mr. Sivarajah Ramanathan, the Mission Director & CEO of the Tamil Nadu Startup and Innovation Mission, during a pitching event hosted by Entrepreneurship Promotion and Incubation Centre (EPIC) at Sri Venkateswara College of Engineering."],
 	},
 ];
