2b1762fbe79c6ab3c278331af4af36d0431910ed
Kesav S J <75158817+Kesav-spec@users.noreply.github.com>
2024-01-10 15:53:39 +0000

    update info

--- data/journey.ts
+++ data/journey.ts
@@ -1,19 +1,35 @@
 export const journeyEN: Journey[] = [
+	
+	{
+		id: 2,
+		title: "Masters in Business Analytics",
+		duration: "Sep 2023 - Present",
+		location: "UCD Michael Smurfit Graduate Business School, Dublin, Ireland",
+		achievements: [],
+	},
 	{
 		id: 1,
 		title: "Bachelor of Technology in Information Technology",
 		duration: "Aug 2019 - Jul 2023",
-		location: "Anna University, India",
+		location: "Sri Venkateswara College of Enginineering, Anna University, India",
 		achievements: [],
 	},
+	
 ];
 
 export const journeyGA: Journey[] = [
 	{
+		id: 2,
+		title: "Masters in Business Analytics",
+		duration: "Sep 2023 - Present",
+		location: "UCD Michael Smurfit Graduate Business School, Dublin, Ireland",
+		achievements: [],
+	},
+	{
 		id: 1,
 		title: "Bachelor of Technology in Information Technology",
 		duration: "Aug 2019 - Jul 2023",
-		location: "Anna University, India",
+		location: "Sri Venkateswara College of Enginineering, Anna University, India",
 		achievements: [],
 	},
 ];
